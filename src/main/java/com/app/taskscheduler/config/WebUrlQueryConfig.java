f077dc3db7132455e146e5e88c3c8ed5ef36052b
VKoparov <valentinkoparov@gmail.com>
2023-07-10 15:05:44 +0300

    fix(env): fix env_var loading

--- src/main/java/com/app/taskscheduler/config/WebUrlQueryConfig.java
+++ src/main/java/com/app/taskscheduler/config/WebUrlQueryConfig.java
@@ -2,14 +2,18 @@ package com.app.taskscheduler.config;
 
 import com.app.taskscheduler.components.TaskExecuteComponent;
 import com.app.taskscheduler.services.WebUrlQueryService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebUrlQueryConfig {
 
+    @Value("${file-paths.web-urls}")
+    private String webUrls;
+
     @Bean
     public TaskExecuteComponent taskExecuteComponent() {
-        return new TaskExecuteComponent(new WebUrlQueryService());
+        return new TaskExecuteComponent(new WebUrlQueryService(webUrls));
     }
 }
