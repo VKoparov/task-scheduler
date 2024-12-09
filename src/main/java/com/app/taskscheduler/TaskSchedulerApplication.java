49485414ab43a78dd426cfb5fb444bb0d67486fb
VKoparov <valentinkoparov@gmail.com>
2024-12-10 00:08:17 +0200

    check(app): test new ci flow

--- src/main/java/com/app/taskscheduler/TaskSchedulerApplication.java
+++ src/main/java/com/app/taskscheduler/TaskSchedulerApplication.java
@@ -8,5 +8,6 @@ public class TaskSchedulerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskSchedulerApplication.class, args);
+		System.out.println("Hello World!");
 	}
 }
